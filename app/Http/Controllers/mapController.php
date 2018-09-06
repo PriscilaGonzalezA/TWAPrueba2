9976d560cbe627b56157349a8f73011013eb7c3f
Priscila Gonzalez <pris.18p@gmail.com>
2018-09-06 12:23:30 -0300

    ubicacion actual

--- app/Http/Controllers/mapController.php
+++ app/Http/Controllers/mapController.php
@@ -8,14 +8,16 @@ use Mapper;
 class mapController extends Controller
 {
     public function inicio(){
-		Mapper::map(-23.657271,  -70.400490, ['zoom' => 15, 'markers' => ['title' => 'MyLocation', 'animation' => 'DROP'], 'clusters' => ['size' => 10, 'center' => true, 'zoom' => 20]]);
-		$direccionesX = [-23.658243, -23.657457, -23.656631];
-		$direccionesY = [-70.400022,  -70.399153, -70.399968];
-		for($i = 0; $i < 3; $i++) {
-			Mapper::marker($direccionesX[$i], $direccionesY[$i], ['symbol' => 'circle', 'scale' => 1000]);
-		}
-		Mapper::marker(-23.657202, -70.401854, ['symbol' => 'circle', 'scale' => 1000]);
 
+		Mapper::map(-31.3359916, -73.8418008,['zoom'=>15, 'locate'=>true, 'center'=> true]);
+	//	$direccionesX = [-23.658243, -23.657457, -23.656631];
+	//	$direccionesY = [-70.400022,  -70.399153, -70.399968];
+     //	for($i = 0; $i < 3; $i++) {
+	//		Mapper::marker($direccionesX[$i], $direccionesY[$i], ['symbol' => 'circle', 'scale' => 1000]);
+	//	}
+        Mapper::marker(-23.6777769, -70.4116944, ['eventMouseOver' => 'console.log("left click");']);
+   //     Mapper::marker(-23.657202, -70.401854, ['symbol' => 'circle', 'scale' => 1000]);
 		return view('welcome');
 	}
+
 }
